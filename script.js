0d0b41039ea1e14368bdbf1388cd03d5f1161482
ldcmartins <ldc.martins3@gmail.com>
2024-09-14 23:27:24 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+
+    const html = document.documentElement  
+    html.classList.toggle("light")
+
+    // pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    // substituir a imagem
+    if(html.classList.contains("light")) {
+
+    // se tiver ligth mode, adicionar a imgagem  light
+        img.setAttribute("src", "assets/avatar.leo.light.png")
+    } else {
+
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/leo.png.ghost.png")
+    }
+}
+
